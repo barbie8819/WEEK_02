70dfb3a7a94065cefd5ce762d532cd8e103b230f
Anshi Patel <anshipatel881@gmail.com>
2025-01-17 11:20:45 +0530

    Mobile Phone

--- Day_01/Level_01/MobilePhoneClass.java
+++ Day_01/Level_01/MobilePhoneClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MobilePhoneClass {
     public static void main(String[] args) {
         // Create a MobilePhone object with attributes
-        MobilePhone mobilePhone = new MobilePhone("SD303" , "Laptop" , 100000);
+        MobilePhone mobilePhone = new MobilePhone("Apple" , "3232" , 100000);
 
         // Display MobilePhone details
         mobilePhone.display();
@@ -28,8 +28,8 @@ class MobilePhone {
 
     // Method to display the details
     public void display() {
-        System.out.println(Brand name  : " + this.brand);
-           System.out.println("model name : " + this.name);
+        System.out.println("Brand name  : " + this.brand);
+           System.out.println("model name : " + this.model);
         System.out.println("price of the phone : " + this.price);
 
    }
