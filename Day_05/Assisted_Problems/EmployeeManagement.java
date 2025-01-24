2570945aa5fda91c45cef583ec629bb70f757097
Anshi Patel <anshipatel881@gmail.com>
2025-01-24 13:11:29 +0530

    Employeemanagement

---
+++ Day_05/Assisted_Problems/EmployeeManagement.java
@@ -0,0 +1,78 @@
+// Main class to demonstrate functionality
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        Employee manager = new Manager("Alice", 101, 90000, 10);
+        Employee developer = new Developer("Bob", 102, 80000, "Java");
+        Employee intern = new Intern("Charlie", 103, 30000, 6);
+
+        manager.displayDetails();
+        developer.displayDetails();
+        intern.displayDetails();
+    }
+}
+// Base class Employee
+class Employee {
+    protected String name;
+    protected int id;
+    protected double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name + ", ID: " + id + ", Salary: $" + salary);
+    }
+}
+
+// Subclass Manager
+class Manager extends Employee {
+    private int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+    }
+}
+
+// Subclass Developer
+class Developer extends Employee {
+    private String programmingLanguage;
+
+    public Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
+
+// Subclass Intern
+class Intern extends Employee {
+    private int duration;
+
+    public Intern(String name, int id, double salary, int duration) {
+        super(name, id, salary);
+        this.duration = duration;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + duration + " months");
+    }
+}
+
+
