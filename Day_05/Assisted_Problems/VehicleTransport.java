1efc2890555b8fe4ae13e691c90176698fbe9164
Anshi Patel <anshipatel881@gmail.com>
2025-01-24 15:27:06 +0530

    Vehicle Problem done

---
+++ Day_05/Assisted_Problems/VehicleTransport.java
@@ -0,0 +1,79 @@
+// Main class to demonstrate functionality
+public class VehicleTransport {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {
+            new Car(200, "Petrol", 5),
+            new Truck(120, "Diesel", 15.0),
+            new Motorcycle(180, "Petrol", false)
+        };
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.displayInfo();
+            System.out.println();
+        }
+    }
+}
+
+// Base class Vehicle
+class Vehicle {
+    protected int maxSpeed;
+    protected String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType);
+    }
+}
+
+// Subclass Car
+class Car extends Vehicle {
+    private int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+// Subclass Truck
+class Truck extends Vehicle {
+    private double loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Subclass Motorcycle
+class Motorcycle extends Vehicle {
+    private boolean hasSidecar;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + (hasSidecar ? "Yes" : "No"));
+    }
+}
+
